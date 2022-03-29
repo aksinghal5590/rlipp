2a87f414a17e6a21e465206131b35d41b2a7ca75
Akshat Singhal <31578650+aksinghal5590@users.noreply.github.com>
2022-03-28 20:14:16 -0700

    Update and rename cv_rlipp.sh to rlipp.sh

--- scripts/rlipp.sh
+++ scripts/rlipp.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 homedir=$1
-ontology="${homedir}/data/training_files_av/ontology_${2}_${3}.txt"
-gene2idfile="${homedir}/data/training_files_av/gene2ind_${2}_${3}.txt"
-cell2idfile="${homedir}/data/training_files_av/cell2ind_${3}.txt"
-test="${homedir}/data/training_files_av/${6}_test_${3}_${4}.txt"
+ontology="${homedir}/data/training_files_av/ontology.txt"
+gene2idfile="${homedir}/data/training_files_av/gene2ind.txt"
+cell2idfile="${homedir}/data/training_files_av/cell2ind.txt"
+test="${homedir}/data/training_files_av/test.txt"
 
-modeldir="${homedir}/models/CNV/model_${2}_${3}_${4}_${5}_${6}"
+modeldir="${homedir}/models/CNV/model"
 
 predicted="${modeldir}/predict.txt"
 sys_output="${modeldir}/rlipp.out"
 gene_output="${modeldir}/gene_rho.out"
 hidden="${modeldir}/hidden"
 
-cpu_count=$7
+cpu_count=$2
 
 neurons=6
 
